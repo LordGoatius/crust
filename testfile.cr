9af67d64166aa58dd9b3cba7e0019f0faef2c00b
Jimmy Ostler <jtostler1@gmail.com>
2024-12-22 02:46:03 -0800

    finish grammar

--- testfile.cr
+++ testfile.cr
@@ -1,46 +1,59 @@
+
+// Hybrid C/Rust style structs
 struct structype {
     my_int i32;
     my_uint u32;
     my_fl f64;
 };
 
+// Enums are tagged unions, and like in rust can be represented using both
+// C style or tagged union style
 enum int_option {
     some i32,
     none,
 };
 
+// Union type can be defined once a ffi is necessary for C lol.
+// For now, built in it is.
+
+// Tuple
 typedef int_flt (u32, f64);
 
+// Static variables, tuple
 static int_flt my_tuple = (15, 34);
 (i32, u32) tuple;
 tuple = (-12, 14);
 
+// All formats can assume C style layout without Rust style
+// struct layout optimization. 
+
+// functions are more C style than rust style.
+// This is both for simplicity and because the constructs that make
+// the arrow better for rust (dyn, generics, etc) do not exist in 
+// this language. Is that better? No, but that's not the point.
 int_option thing() {
     u32 my_var;
     my_var = 14;
-    int_option my_opt = none;
+    int_option my_opt = none; // in definition 
 
-    char* thing = "thing";
-
-    if let int_option.some value = my_opt {
-        return enum int_option.none;
+    if let some value = my_opt {
+        return none;
     } else {
-        return enum int_option.some my_var;
+        return my_var;
     }
 }
 
 structype thing_1() {
     struct structype thing = {
-        my_int = -255;
-        my_uint = 13;
-        my_fl = 12.4;
+        my_int = -255,
+        my_uint = 13,
+        my_fl = 12.4,
     };
     thing.my_int = 12;
-    (&thing)->my_int = 13;
-    return struct structype {
-        my_int = -255;
-        my_uint = 13;
-        my_fl = 12.4;
+    return {
+        my_int = -255,
+        my_uint = 13,
+        my_fl = 12.4,
     };
 
 }
@@ -50,17 +63,20 @@ void thing_2() {
     u32* ptr_u32 = (u32*) arr;
 }
 
+// We also get a match expression
 void match() {
-    match enum int_option.none {
-        int_option.none: {
+    match none {
+        none: {
             return;
         },
-        int_option.some 4: {
+        some 4: {
             return;
         },
-        int_option.some _: {
+        some _: {
             return;
         },
-    }
+    } // match needs no semicolon, just like `if` and `for`
     return;
 }
+
+
